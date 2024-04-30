43d2abaf598c80bd6b235d62851d225f4f828635
Jonatan Oliveira <jonatan.oliveira@opah.com.br>
2024-04-29 21:30:49 -0300

    feat: add category item and list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Search from "./_components/search";
 import Header from "./_components/header";
+import CategoryList from "./_components/category-list";
 
 const Home = () => {
   return (
@@ -7,6 +8,9 @@ const Home = () => {
       <Header />
       <div className="px-5 pt-6">
         <Search />
+      </div>
+      <div className="px5 pt-6">
+        <CategoryList />
       </div>
     </>
   );
