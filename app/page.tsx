4e598ca14d8b243605f098e2cec406ff5fa7eec0
Jonatan Oliveira <jonatan.oliveira@opah.com.br>
2024-04-29 21:00:56 -0300

    feat: add search inital component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Header from "./_components/Header";
+import Search from "./_components/search";
+import Header from "./_components/header";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 };
 
 export default Home;
