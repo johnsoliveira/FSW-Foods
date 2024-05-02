19e19f71fa5c5bdcd9b965ccd12ebba9056852ca
Jonatan Oliveira <jonatan.oliveira@opah.com.br>
2024-05-02 18:41:27 -0300

    feat: finished restaurant page

--- app/product/[id]/_components/product-detail.tsx
+++ app/product/[id]/_components/product-detail.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Cart from "@/app/_components/cart";
+import DeliveryInfo from "@/app/_components/delivery-info";
 import DiscountBadge from "@/app/_components/discount-badge";
 import ProductList from "@/app/_components/product-list";
 import {
@@ -14,7 +15,6 @@ import {
   AlertDialogTitle,
 } from "@/app/_components/ui/alert-dialog";
 import { Button } from "@/app/_components/ui/button";
-import { Card } from "@/app/_components/ui/card";
 import {
   Sheet,
   SheetContent,
@@ -27,12 +27,7 @@ import {
   formatCurrency,
 } from "@/app/_helpers/price";
 import { Prisma } from "@prisma/client";
-import {
-  BikeIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  TimerIcon,
-} from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 import { useContext, useState } from "react";
 
@@ -59,8 +54,6 @@ const ProductDetail = ({
     useState(false);
 
   const { addProductToCart, products } = useContext(CartContext);
-
-  console.log(products, "Produtos");
 
   const addToCart = ({ emptyCart }: { emptyCart?: boolean }) => {
     addProductToCart({ product, quantity, emptyCart });
@@ -136,31 +129,7 @@ const ProductDetail = ({
           </div>
         </div>
         <div className="px-5">
-          <Card className="mt-6 flex justify-around py-3">
-            <div className="flex flex-col items-center">
-              <div className="flex items-center gap-2 text-muted-foreground">
-                <span className="text-xs">Entrega</span>
-                <BikeIcon size={14} />
-              </div>
-              {Number(product.restaurant.deliveryFee) > 0 ? (
-                <p className="text-xs font-semibold">
-                  {formatCurrency(Number(product.restaurant.deliveryFee))}
-                </p>
-              ) : (
-                <p className="text-xs font-semibold">Grátis</p>
-              )}
-            </div>
-            <div className="flex flex-col items-center">
-              <div className="flex items-center gap-2 text-muted-foreground">
-                <span className="text-xs">Tempo</span>
-                <TimerIcon size={14} />
-              </div>
-
-              <p className="text-xs font-semibold">
-                {product.restaurant.deliveryTimeMinutes} min
-              </p>
-            </div>
-          </Card>
+          <DeliveryInfo restaurant={product.restaurant} />
         </div>
         <div className="mt-6 space-y-3 px-5">
           <h3 className="font-semibold">Sobre</h3>
--- app/restaurant/[id]/page.tsx
+++ app/restaurant/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { db } from "@/app/_lib/prisma";
 import { notFound } from "next/navigation";
 import RestaurantImage from "./_components/restaurant-image";
-
+import Image from "next/image";
+import { StarIcon } from "lucide-react";
+import DeliveryInfo from "@/app/_components/delivery-info";
+import ProductList from "@/app/_components/product-list";
 interface RestaurantePageProps {
   params: {
     id: string;
@@ -13,6 +16,37 @@ const RestaurantePage = async ({ params: { id } }: RestaurantePageProps) => {
     where: {
       id: id,
     },
+    include: {
+      categories: {
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          products: {
+            where: {
+              restaurantId: id,
+            },
+            include: {
+              restaurant: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      products: {
+        take: 10,
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!restaurant) {
@@ -20,9 +54,55 @@ const RestaurantePage = async ({ params: { id } }: RestaurantePageProps) => {
   }
 
   return (
-    <>
+    <div>
       <RestaurantImage restaurant={restaurant} />
-    </>
+      <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl rounded-tr-3xl bg-white">
+        <div className="flex items-center justify-between px-5 pt-5">
+          <div className="flex items-center gap-[0.375rem]">
+            <div className="relative h-8 w-8">
+              <Image
+                src={restaurant.imageUrl}
+                alt={restaurant.name}
+                fill
+                className="rounded-full object-cover"
+              />
+            </div>
+            <h2 className="text-xl font-semibold">{restaurant.name}</h2>
+          </div>
+          <div className="flex items-center gap-[3px] rounded-full bg-foreground px-2 py-[2px] text-white">
+            <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
+            <span className="text-xs font-semibold">5.0</span>
+          </div>
+        </div>
+      </div>
+      <div className="px-5">
+        <DeliveryInfo restaurant={restaurant} />
+      </div>
+      <div className="mt-6 flex gap-4 overflow-x-scroll px-5  [&::-webkit-scrollbar]:hidden">
+        {restaurant.categories.map((category) => (
+          <div
+            className="min-w-[167px] rounded-lg bg-[#f4f4f4] text-center"
+            key={category.id}
+          >
+            <span className="text-xs text-muted-foreground">
+              {category.name}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div className="mt-6 space-y-4">
+        <h2 className="px-5 font-semibold">Mais Pedidos</h2>
+        {/*TODO: mostrar os produtos mais pedidos*/}
+        <ProductList products={restaurant.products} />
+      </div>
+      {restaurant.categories.map((category) => (
+        <div className="mt-6 space-y-4" key={category.id}>
+          <h2 className="px-5 font-semibold">{category.name}</h2>
+          {/*TODO: mostrar os produtos mais pedidos*/}
+          <ProductList products={category.products} />
+        </div>
+      ))}
+    </div>
   );
 };
 
