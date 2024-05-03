00af067f599c22dbd37b53f9c589d617c857b54e
Jonatan Oliveira <jonatan.oliveira@opah.com.br>
2024-05-03 11:28:40 -0300

    feat: add recommended restaurants page

---
+++ app/restaurants/recommended/page.tsx
@@ -0,0 +1,25 @@
+import Header from "@/app/_components/header";
+import RestaurantItem from "@/app/_components/restaurant-item";
+import { db } from "@/app/_lib/prisma";
+
+const RecommendedRestaurants = async () => {
+  const restaurants = await db.restaurant.findMany({});
+
+  return (
+    <>
+      <Header />
+      <div className="mt-6 space-y-4 px-5">
+        <h2 className="mb-6 text-lg font-semibold">
+          Restaurantes Recomendados
+        </h2>
+        {restaurants.map((restaurant) => (
+          <div key={restaurant.id}>
+            <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default RecommendedRestaurants;
